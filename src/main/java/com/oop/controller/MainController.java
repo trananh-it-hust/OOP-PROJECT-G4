6b018855dc34b347cd7e4adce0d597382f774bdc
trananh-it-hust <haianhtranmanh@gmail.com>
2024-04-16 21:13:14 +0700

    haianh update

--- src/main/java/com/oop/controller/MainController.java
+++ src/main/java/com/oop/controller/MainController.java
@@ -1,14 +1,26 @@
 package com.oop.controller;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.util.List;
+
+import com.oop.model.Item;
+import com.oop.model.SearchModel;
+import com.opencsv.exceptions.CsvValidationException;
 
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class MainController {
@@ -28,19 +40,74 @@ public class MainController {
         stage.show();
     }
 
-    public void switchToSearchResults(ActionEvent event) throws IOException {
+    public void switchToSearchResults(Event event) throws IOException, CsvValidationException, ParseException {
+        List<Item> searchResults = handleSearch();
         root = FXMLLoader.load(getClass().getResource("/view/SearchResults.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
+        for (Item item : searchResults) {
+            VBox itemCard = Item.renderUI(item);
+            ((VBox) scene.lookup("#searchResults")).getChildren().add(itemCard);
+        }
         stage.setScene(scene);
         stage.show();
     }
 
     @FXML
-    public void handleSearch() {
-        System.out.println("Search button clicked");
-        // get data from the search field
+    public List<Item> handleSearch() throws CsvValidationException, IOException, ParseException {
         String searchQuery = searchField.getText();
         System.out.println("Search query: " + searchQuery);
+        List<Item> items = Item.readItemsFromCSV();
+        List<Item> searchResults = SearchModel.searchByTitle(items, searchQuery);
+        return searchResults;
+    }
+
+    public void addSuggestions(List<String> suggestions) throws IOException {
+        VBox suggestionsBox;
+        if (scene != null) {
+            suggestionsBox = (VBox) scene.lookup("#suggestions");
+            suggestionsBox.getChildren().clear();
+        } else {
+            root = FXMLLoader.load(getClass().getResource("/view/Main.fxml"));
+            scene = new Scene(root);
+            suggestionsBox = (VBox) scene.lookup("#suggestions");
+            suggestionsBox.getChildren().clear();
+        }
+        for (String suggestion : suggestions) {
+            System.out.println("Suggestion: " + suggestion);
+            VBox suggestionField = new VBox();
+            Label suggestionLabel = new Label("a" + suggestion);
+            suggestionField.getChildren().add(suggestionLabel);
+            suggestionsBox.getChildren().add(suggestionField);
+        }
+    }
+
+    public void initialize() {
+        // Thêm lắng nghe sự kiện cho TextField khi scene được tạo
+        searchField.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                // Kiểm tra nếu phím được ấn là phím Enter
+                if (event.getCode().equals(KeyCode.ENTER)) {
+                    try {
+                        switchToSearchResults(event);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (CsvValidationException e) {
+                        e.printStackTrace();
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    String searchQuery = searchField.getText();
+                    List<String> suggestionsResults = SearchModel.GetSuggestion(searchQuery);
+                    try {
+                        addSuggestions(suggestionsResults);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
     }
 }
