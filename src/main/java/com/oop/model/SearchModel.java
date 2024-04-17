d649eb047dd5d027e41129f9d6feed61e49e908b
Saphyiera <nguyenduc281107@gmail.com>
2024-04-17 22:23:26 +0700

    add get suggestion

--- src/main/java/com/oop/model/SearchModel.java
+++ src/main/java/com/oop/model/SearchModel.java
@@ -1,7 +1,14 @@
 package com.oop.model;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class SearchModel {
     public static List<Item> searchByTitle(List<Item> items, String keyword) {
@@ -15,11 +22,22 @@ public class SearchModel {
     }
 
     public static List<String> GetSuggestion(String keyword) {
+                        //from suggestion.json
+
         List<String> suggestions = new ArrayList<>();
-        suggestions.add("Bitcoin");
-        suggestions.add("Ethereum");
-        suggestions.add("Ripple");
-        suggestions.add("Litecoin");
+        Object obj = null;
+        try {
+            obj = new JSONParser().parse(new FileReader("/src/main/resources/data/suggestion.json"));
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+        JSONObject jo = (JSONObject) obj;
+        JSONArray ja = (JSONArray) jo.get("du_lieu");
+
+        for (int i = 0; i < ja.size(); i++) {
+            suggestions.add((String)ja.get(i));
+        }
+
         return suggestions;
     }
 }
