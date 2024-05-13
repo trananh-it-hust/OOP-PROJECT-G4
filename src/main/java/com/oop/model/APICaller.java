059ccf76d689d9222efe50972019073a1d72eeb4
Saphyiera <nguyenduc281107@gmail.com>
2024-05-14 06:25:35 +0700

    new api caller for entities search

--- src/main/java/com/oop/model/APICaller.java
+++ src/main/java/com/oop/model/APICaller.java
@@ -9,7 +9,10 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -20,7 +23,7 @@ public class APICaller {
     // Lấy gợi ý
     public static List<String> querySuggest(String input) throws URISyntaxException, IOException, ParseException {
         List<String> res = new ArrayList<String>();
-        StringBuffer content = connectAndGetRawData("http://localhost:8000/suggestion?data=", input);
+        StringBuffer content = connectAndGetRawData("GET", "http://localhost:8000/suggestion?data=", input);
         JSONParser parser = new JSONParser();
         JSONObject jo = (JSONObject) parser.parse(content.toString());
         JSONArray ja = (JSONArray) jo.get("result");
@@ -33,7 +36,7 @@ public class APICaller {
     // Lấy tìm kiếm
     public static List<Item> getSearchResult(String input) throws ParseException, IOException, URISyntaxException {
         List<Item> results = new ArrayList<Item>();
-        StringBuffer jsonContent = connectAndGetRawData("http://localhost:8000/search?q=", input);
+        StringBuffer jsonContent = connectAndGetRawData("GET", "http://localhost:8000/search?q=", input);
         String s = jsonContent.toString().replace("NaN", "\"None\"");
         JSONObject jo = (JSONObject) new JSONParser().parse(s);
         JSONArray ja = (JSONArray) jo.get("results");
@@ -55,15 +58,44 @@ public class APICaller {
         return results;
     }
 
+    // Get entities from paragraph
+    public static HashMap<String, Set<String>> getEntities(String input)
+            throws IOException, URISyntaxException, ParseException {
+        HashMap<String, Set<String>> result = new HashMap<String, Set<String>>();
+        StringBuffer jsonContent = connectAndGetRawData("POST", "http://localhost:8000/predict?text=", input);
+        String s = jsonContent.toString();
+        JSONObject jo = (JSONObject) new JSONParser().parse(s);
+        JSONArray ja = (JSONArray) jo.get("entities");
+        for (Object joja : ja) {
+            if (joja instanceof JSONObject) {
+                JSONObject jojajo = (JSONObject) joja;
+                for (Object key : jojajo.keySet()) {
+                    JSONArray jaja = (JSONArray) jojajo.get(key);
+                    Set<String> items = new HashSet<String>();
+                    for (Object object : jaja) {
+                        JSONArray jojojo = (JSONArray) object;
+                        for (Object object2 : jojojo) {
+                            if (object2 instanceof String) {
+                                items.add((String) object2);
+                            }
+                        }
+                    }
+                    result.put((String) key, items);
+                }
+            }
+        }
+        return result;
+    }
+
     // Goi api va lay ket qua vao buffer
-    public static StringBuffer connectAndGetRawData(String urlString, String input)
+    public static StringBuffer connectAndGetRawData(String methodType, String urlString, String input)
             throws IOException, URISyntaxException {
         StringBuffer content = new StringBuffer();
         String parsedInput = URLEncoder.encode(input, "UTF-8");
         URI uri = new URI(urlString + parsedInput);
         URL url = uri.toURL();
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
+        con.setRequestMethod(methodType);
         con.setRequestProperty("Content-type", "application/json");
         con.setConnectTimeout(5000);
         con.setReadTimeout(5000);
