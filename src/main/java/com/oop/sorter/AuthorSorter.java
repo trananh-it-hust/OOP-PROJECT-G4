146950a1ee65990ab95bc84247261f9d8b861266
vietcuong <vietcuong2k182@gmail.com>
2024-05-21 20:13:42 +0700

    check

---
+++ src/main/java/com/oop/sorter/AuthorSorter.java
@@ -0,0 +1,10 @@
+package com.oop.sorter;
+
+import java.util.List;
+
+public class AuthorSorter implements ISorter{
+    @Override
+    public List<Object> sort(List<Object> list) {
+        return null;
+    }
+}
---
+++ src/main/java/com/oop/sorter/ISorter.java
@@ -0,0 +1,7 @@
+package com.oop.sorter;
+
+import java.util.List;
+
+public interface ISorter {
+    List<Object> sort(List<Object> list);
+}
