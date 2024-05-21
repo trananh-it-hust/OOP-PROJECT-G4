85e2e1603c84628e6f86b1b468f637b8243f8266
vietcuong <vietcuong2k182@gmail.com>
2024-05-21 20:13:42 +0700

    check

--- src/main/java/com/oop/model/NetWorkException.java
+++ src/main/java/com/oop/model/NetWorkException.java
@@ -4,4 +4,7 @@ public class NetWorkException extends Exception{
     public NetWorkException(String msg){
         super(msg);
     }
+    public NetWorkException(Throwable cause){
+        super(cause);
+    }
 }
