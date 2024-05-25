ea077a254e0482a659d4cf1c55b80f862d1c8039
vietcuong <vietcuong2k182@gmail.com>
2024-05-25 19:31:43 +0700

    cap nhat sort

--- src/main/java/com/oop/sorter/DateSorter.java
+++ src/main/java/com/oop/sorter/DateSorter.java
@@ -1,10 +1,30 @@
 package com.oop.sorter;
 
+import com.oop.model.Item;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
-public class DateSorter implements ISorter{
+public class DateSorter implements ISorter<Item> {
+
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
     @Override
-    public List<Object> sort(List<Object> list) {
-        return null;
+    public List<Item> sort(List<Item> list) {
+        List<Item> sortedList = new ArrayList<>(list);
+        Collections.sort(sortedList, new Comparator<Item>() {
+            @Override
+            public int compare(Item item1, Item item2) {
+                try {
+                    return sdf.parse(String.valueOf(String.valueOf(item1.getCreationDate()))).compareTo(sdf.parse(String.valueOf(item2.getCreationDate())));
+                } catch (ParseException e) {
+                    throw new IllegalArgumentException("Invalid date format", e);
+                }
+            }
+        });
+        return sortedList;
     }
 }
--- src/main/java/com/oop/sorter/ISorter.java
+++ src/main/java/com/oop/sorter/ISorter.java
@@ -2,6 +2,6 @@ package com.oop.sorter;
 
 import java.util.List;
 
-public interface ISorter {
-    List<Object> sort(List<Object> list);
+public interface ISorter<T extends Comparable<T>> {
+    List<T> sort(List<T> list);
 }
