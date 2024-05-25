65cf7b758d8d6ab7a55c410ba5e3d2f2a530a9a6
vietcuong <vietcuong2k182@gmail.com>
2024-05-26 00:25:49 +0700

    them switchController

--- src/main/java/com/oop/sorter/AuthorSorter.java
+++ src/main/java/com/oop/sorter/AuthorSorter.java
@@ -1,11 +1,29 @@
 package com.oop.sorter;
 
+import com.oop.model.Item;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public class AuthorSorter implements ISorter{
+public class AuthorSorter implements ISorter<Item> {
 
     @Override
-    public List sort(List list) {
-        return null;
+    public List<Item> sort(List<Item> list) {
+        List<Item> sortedList = new ArrayList<>(list);
+        sortedList.sort((item1, item2) -> {
+            try {
+                return item1.getAuthor().compareTo(item2.getAuthor());
+            } catch (NullPointerException e) {
+                if (item1.getAuthor() == null && item2.getAuthor() == null) {
+                    return 0;
+                }
+                if (item1.getAuthor() == null) {
+                    return -1;
+                }
+                return 1;
+            }
+        });
+        return sortedList;
     }
 }
